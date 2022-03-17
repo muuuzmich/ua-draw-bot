5b667a715291157a4b665a84d933d879c169de4b
Misha <muuuzmich@gmail.com>
2022-03-17 21:04:30 +0200

    server port changed to env port

--- src/core/server.ts
+++ src/core/server.ts
@@ -9,7 +9,7 @@ export default class Server {
   }
 
   public listen(): void {
-    const port = 3000;
+    const port = process.env.PORT || 3000;
     this.app.listen(port, () => console.log(`Server is listening at ${port}`));
   }
 }
